5c15cc7b164f379fa601d0c2545a9bea56dd150f
Dmitry Kozlov <thaidem19@gmail.com>
2020-10-14 17:17:22 +0300

    Subtyping

--- src/main/java/generics/ParameterizedClass.java
+++ src/main/java/generics/ParameterizedClass.java
@@ -2,16 +2,21 @@ package generics;
 
 public class ParameterizedClass {
   public static void main(String[] args) {
-    Info<String> info1 = new Info<String>("hi");
-    System.out.println(info1);
-    String s = info1.getValue();
+//    Info<String> info1 = new Info<String>("hi");
+//    System.out.println(info1);
+//    String s = info1.getValue();
 
     Info<Integer> info2 = new Info<Integer>(10);
     System.out.println(info2);
     Integer i = info2.getValue();
+
+    Info<Double> info3 = new Info<Double>(3.14);
+    System.out.println(info3);
+    Double d = info3.getValue();
   }
 }
-class Info<T> {
+class Info<T extends Number> {
+//class Info<T extends Number&I1&I2> {
   private T value;
 
   public Info(T value) {
@@ -27,3 +32,6 @@ class Info<T> {
     return value;
   }
 }
+
+interface I1 {}
+interface I2 {}
---
+++ src/main/java/generics/Test2.java
@@ -0,0 +1,21 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2 {
+  public static void main(String[] args) {
+//    X x = new Y();
+//    List<X> list1 = new ArrayList<X>();
+
+    List<Number> list = new ArrayList<Number>();
+  }
+}
+
+class X {
+
+}
+
+class Y extends X {
+
+}
