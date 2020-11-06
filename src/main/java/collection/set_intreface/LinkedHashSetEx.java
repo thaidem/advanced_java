328cfb97afe67ec283f4bbae244833fb33711642
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-06 15:10:51 +0300

    LinkedHashSet

---
+++ src/main/java/collection/set_intreface/LinkedHashSetEx.java
@@ -0,0 +1,17 @@
+package collection.set_intreface;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEx {
+  public static void main(String[] args) {
+    LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+    lhs.add(5);
+    lhs.add(3);
+    lhs.add(1);
+    lhs.add(10);
+    lhs.add(8);
+    System.out.println(lhs);
+    lhs.remove(1);
+    System.out.println(lhs.contains(1));
+  }
+}
