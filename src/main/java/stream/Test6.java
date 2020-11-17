f27b076d434cd03c0c44d7b52fff7d27b0a19689
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-17 15:41:51 +0300

    distinct

--- src/main/java/stream/Test6.java
+++ src/main/java/stream/Test6.java
@@ -5,9 +5,15 @@ import java.util.stream.Stream;
 public class Test6 {
   public static void main(String[] args) {
     Stream<Integer> stream1 = Stream.of(1,2,3,4,5,1,2,3);
-    Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
-    Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
-    Stream<Integer> stream4 = Stream.concat(stream2, stream3);
-    stream4.forEach(System.out::println);
+
+
+//    Stream<Integer> stream2 = Stream.of(1,2,3,4,5);
+//    Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
+//    Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+//    stream4.forEach(System.out::println);
+
+    Stream<Integer> stream5 = Stream.of(1,2,3,4,5,1,2,3);
+    stream5.distinct().forEach(System.out::println);
+
   }
 }
