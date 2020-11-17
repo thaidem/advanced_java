4e81d1aaef177744123a6f8e80544b0d780e2e1d
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-17 16:57:29 +0300

    groupingBy partitioningBy

---
+++ src/main/java/stream/Test8.java
@@ -0,0 +1,43 @@
+package stream;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Test8 {
+  public static void main(String[] args) {
+    Student st1 = new Student("Ivan", 'm', 20, 3, 8.7);
+    Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+    Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+    Student st4 = new Student("Petr", 'm', 35, 4, 7);
+    Student st5 = new Student("Mariya", 'f', 23, 3, 9.1);
+    List<Student> students = new ArrayList<>();
+    students.add(st1);
+    students.add(st2);
+    students.add(st3);
+    students.add(st4);
+    students.add(st5);
+
+//    Map<Integer, List<Student>> map = students.stream().map(e -> {
+//      e.setName(e.getName().toUpperCase());
+//      return e;
+//    })
+//            .collect(Collectors.groupingBy(e -> e.getCourse()));
+//
+//    for(Map.Entry<Integer, List<Student>> entry : map.entrySet()) {
+//      System.out.println(entry.getKey() + " : " + entry.getValue().toString());
+//    }
+
+    Map<Boolean, List<Student>> map =
+            students.stream().collect(Collectors.partitioningBy(e -> e.getAverageGrade() > 7));
+
+    for(Map.Entry<Boolean, List<Student>> entry : map.entrySet()) {
+      System.out.println(entry.getKey() + " : " + entry.getValue().toString());
+    }
+
+
+  }
+}
