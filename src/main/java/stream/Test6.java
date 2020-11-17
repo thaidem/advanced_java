8ee1ee229eee401f9f240084b5f7ff07bd5e4577
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-17 15:49:28 +0300

    peek

--- src/main/java/stream/Test6.java
+++ src/main/java/stream/Test6.java
@@ -15,7 +15,9 @@ public class Test6 {
     Stream<Integer> stream5 = Stream.of(1,2,3,4,5,1,2,3);
 //    stream5.distinct().forEach(System.out::println);
 //    System.out.println(stream5.count());
-    System.out.println(stream5.distinct().count());
+//    System.out.println(stream5.distinct().count());
+
+    System.out.println(stream5.distinct().peek(System.out::println).count());
 
 
   }
