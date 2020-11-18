06563368fcab53fccf1a5ab713b34c31fd816557
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-18 14:06:38 +0300

    mapToInt

--- src/main/java/stream/Test9.java
+++ src/main/java/stream/Test9.java
@@ -18,6 +18,22 @@ public class Test9 {
     students.add(st4);
     students.add(st5);
 
+//    int sum = students.stream().mapToInt(e -> e.getCourse()).sum();
+//    System.out.println(sum);
+//    double average = students.stream().mapToInt(e -> e.getCourse()).average().getAsDouble();
+//    System.out.println(average);
+    int min = students.stream().mapToInt(e -> e.getCourse()).min().getAsInt();
+    System.out.println(min);
+    int max = students.stream().mapToInt(e -> e.getCourse()).max().getAsInt();
+    System.out.println(max);
+
+
+//    List<Integer> courses = students.stream().mapToInt(e ->
+//            e.getCourse())
+//            .boxed()
+//            .collect(Collectors.toList());
+//    System.out.println(courses);
+
 //    Student min  = students.stream().min((x,y) -> x.getCourse()- y.getCourse()).get();
 //    System.out.println(min);
 //
@@ -28,10 +44,10 @@ public class Test9 {
 //    System.out.println(list);
 //    students.stream().filter(e -> e.getAverageGrade() < 9).limit(2).forEach(System.out::println);
 
-    students.stream().filter(e -> e.getAge() > 20).forEach(System.out::println);
-    System.out.println("---------------------------");
-    students.stream().filter(e -> e.getAge() > 20).limit(2).forEach(System.out::println);
-    System.out.println("---------------------------");
-    students.stream().filter(e -> e.getAge() > 20).skip(2).forEach(System.out::println);
+//    students.stream().filter(e -> e.getAge() > 20).forEach(System.out::println);
+//    System.out.println("---------------------------");
+//    students.stream().filter(e -> e.getAge() > 20).limit(2).forEach(System.out::println);
+//    System.out.println("---------------------------");
+//    students.stream().filter(e -> e.getAge() > 20).skip(2).forEach(System.out::println);
   }
 }
