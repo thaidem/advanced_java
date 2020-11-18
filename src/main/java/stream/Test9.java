fc468275647184c6473c3580a7547d1738369c75
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-18 13:46:44 +0300

    skip

--- src/main/java/stream/Test9.java
+++ src/main/java/stream/Test9.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Test9 {
   public static void main(String[] args) {
-    Student st1 = new Student("Ivan", 'm', 20, 3, 8.7);
+    Student st1 = new Student("Ivan", 'm', 22, 3, 8.7);
     Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
     Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
     Student st4 = new Student("Petr", 'm', 35, 4, 7);
@@ -24,8 +24,14 @@ public class Test9 {
 //    Student max  = students.stream().max((x,y) -> x.getCourse()- y.getCourse()).get();
 //    System.out.println(max);
 
-    List<Student> list = students.stream().filter(e -> e.getAge() > 20).limit(3).collect(Collectors.toList());
-    System.out.println(list);
-    students.stream().filter(e -> e.getAverageGrade() < 9).limit(2).forEach(System.out::println);
+//    List<Student> list = students.stream().filter(e -> e.getAge() > 20).limit(3).collect(Collectors.toList());
+//    System.out.println(list);
+//    students.stream().filter(e -> e.getAverageGrade() < 9).limit(2).forEach(System.out::println);
+
+    students.stream().filter(e -> e.getAge() > 20).forEach(System.out::println);
+    System.out.println("---------------------------");
+    students.stream().filter(e -> e.getAge() > 20).limit(2).forEach(System.out::println);
+    System.out.println("---------------------------");
+    students.stream().filter(e -> e.getAge() > 20).skip(2).forEach(System.out::println);
   }
 }
